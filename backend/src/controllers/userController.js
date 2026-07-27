ce86a4e2c033b8dc7b46640e17eeeaf1b28ff672
Riyaz <r9652308234@gmail.com>
2026-07-27 15:26:52 +0530

    bug: Custom apierror not working

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -10,6 +10,14 @@ const registerUser = asyncHandler(async (req, res) => {
 
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+
+    const result = await userService.loginUser(req.body);
+
+    res.status(200).json(result);
+
+});
+
 module.exports = {
-    registerUser,
+    registerUser,loginUser
 };
--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -5,44 +5,40 @@ const pool = require("../database/db");
 const ApiError = require("../utils/ApiError");
 
 async function registerUser(userData) {
+  const { name, email, password, role } = userData;
 
-    const { name, email, password, role } = userData;
+  if (
+    !name ||
+    !name.trim() ||
+    !email ||
+    !email.trim() ||
+    !password ||
+    !password.trim() ||
+    !role ||
+    !role.trim()
+  ) {
+    throw new ApiError(400, "All fields are required.");
+  }
 
-
-    if (
-    !name || !name.trim() ||
-    !email || !email.trim() ||
-    !password || !password.trim() ||
-    !role || !role.trim()
-) {
-    throw new ApiError(
-        400,
-        "All fields are required."
-    );
-}
-
-    // Step 1: Check if email already exists
-    const existingUser = await pool.query(
-        `
+  // Step 1: Check if email already exists
+  const existingUser = await pool.query(
+    `
         SELECT id
         FROM users
         WHERE email = $1;
         `,
-        [email]
-    );
+    [email],
+  );
 
-    // Step 2: Stop registration if email exists
-    if (existingUser.rows.length > 0) {
-         throw new ApiError(
-        409,
-        "Email already registered."
-    );
-    }
+  // Step 2: Stop registration if email exists
+  if (existingUser.rows.length > 0) {
+    throw new ApiError(409, "Email already registered.");
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // Step 3: Insert new user
-    const result = await pool.query(
-        `
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // Step 3: Insert new user
+  const result = await pool.query(
+    `
         INSERT INTO users (name, email, password, role)
         VALUES ($1, $2, $3, $4)
         RETURNING id,
@@ -51,20 +47,66 @@ async function registerUser(userData) {
                 role,
                 created_at;
         `,
-        [name, email, hashedPassword, role]
-    );
+    [name, email, hashedPassword, role],
+  );
 
-    // Step 4: Return inserted user
-    return {
-        success: true,
-        message: "User registered successfully.",
-        user: result.rows[0],
-    };
+  // Step 4: Return inserted user
+  return {
+    success: true,
+    message: "User registered successfully.",
+    user: result.rows[0],
+  };
 }
 
-module.exports = {
-    registerUser,
+const loginUser = async (userData) => {
+  const { email, password } = userData;
+
+  if (!email || !email.trim() || !password || !password.trim()) {
+    throw new ApiError(400, "Email and password are required.");
+   }
+    const result = await pool.query(
+      `
+    SELECT
+        id,
+        name,
+        email,
+        password,
+        role,
+        created_at
+    FROM users
+    WHERE email = $1;
+    `,
+      [email],
+    );
+
+    if (result.rows.length === 0) {
+      throw new ApiError(401, "Invalid email or password.");
+    }
+  
+    const user = result.rows[0];
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+     if (!isPasswordValid) {
+        throw new ApiError(
+            401,
+            "Invalid email or password."
+        );
+    }
+
+    const {
+        password: hashedPassword,
+        ...safeUser
+    } = user;
+
+    return {
+        success: true,
+        message: "Login successful.",
+        user: safeUser
+    };
+
 };
 
-
-
+module.exports = {
+  registerUser,
+  loginUser,
+};
